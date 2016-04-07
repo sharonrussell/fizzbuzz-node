d9e0ad4dd3f342359d3dd9d00801bedaabdd8acb
sharonrussell <sharon.russell@comparethemarket.com>
2016-04-07 18:51:28 +0100

    Passing code for 15 should return FizzBuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,9 +1,28 @@
 function Fizzbuzz() {}
 
-Fizzbuzz.prototype.getNumber = function(number) {
+function isDivisibleBy3(number){
     if (number % 3 === 0){
+        return true;
+    }
+
+    return false;
+};
+
+function isDivisibleBy5(number){
+    if(number % 5 === 0){
+        return true;
+    }
+
+    return false;
+};
+
+Fizzbuzz.prototype.getNumber = function(number) {
+
+    if (isDivisibleBy3(number) && isDivisibleBy5(number)){
+        return 'FizzBuzz';
+    } else if (isDivisibleBy3(number)){
         return 'Fizz';
-    } else if (number % 5 === 0){
+    } else if (isDivisibleBy5(number)){
         return 'Buzz'
     } else {
         return number;
